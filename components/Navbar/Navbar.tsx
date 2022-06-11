ffb170dab38828657ddd083cf1ffbc5c6f1ddb19
Andrei Remus Plesa <80645758+remusplesa@users.noreply.github.com>
2022-06-11 21:05:01 +0300

    navbar improvements

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import { Container, Title, Button, Group } from "@mantine/core";
+import { Container, Title, Button, Group, Box } from "@mantine/core";
 
 const Navbar = ({ title }: NavbarProps) => {
   const router = useRouter();
@@ -9,10 +9,20 @@ const Navbar = ({ title }: NavbarProps) => {
       py={16}
       style={{ display: "flex", flexDirection: "row", alignItems: "center" }}
     >
-      <Title order={1}>{title}</Title>
+      <Box onClick={() => router.push("/")} style={{cursor: "pointer"}}>
+        <Title order={1}>{title}</Title>
+      </Box>
       <Group align="right" style={{ marginLeft: "auto" }}>
         <Button variant="outline" compact onClick={() => router.push("/login")}>
           Log in{" "}
+        </Button>
+        <Button
+          variant="outline"
+          color={"red"}
+          compact
+          onClick={() => router.push("/logout")}
+        >
+          Log out{" "}
         </Button>
       </Group>
     </Container>
